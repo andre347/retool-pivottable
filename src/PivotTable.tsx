0b1a1e90e963989a079604eb81e98826c97de309
Andre de Vries <aedevries19@gmail.com>
2024-04-18 16:35:07 +0100

    first commit

---
+++ src/PivotTable.tsx
@@ -0,0 +1,184 @@
+import React from 'react'
+import { type FC } from 'react'
+import PivotTableUI from 'react-pivottable/PivotTableUI'
+import 'react-pivottable/pivottable.css'
+
+import { Retool } from '@tryretool/custom-component-support'
+
+export const PivotTable: FC = () => {
+  const [pivotData, setPivotData] = Retool.useStateArray({
+    name: 'pivotData',
+    initialValue: [
+      {
+        Date: '2024-01-02',
+        Store: 'North',
+        BookGenre: 'Fiction',
+        Author: 'John Doe',
+        UnitsSold: 3,
+        Revenue: 45
+      },
+      {
+        Date: '2024-01-03',
+        Store: 'North',
+        BookGenre: 'Non-Fiction',
+        Author: 'Jane Doe',
+        UnitsSold: 2,
+        Revenue: 55
+      },
+      {
+        Date: '2024-01-04',
+        Store: 'East',
+        BookGenre: 'Fiction',
+        Author: 'Emily White',
+        UnitsSold: 4,
+        Revenue: 60
+      },
+      {
+        Date: '2024-01-05',
+        Store: 'South',
+        BookGenre: 'Non-Fiction',
+        Author: 'James Smith',
+        UnitsSold: 1,
+        Revenue: 25
+      },
+      {
+        Date: '2024-01-06',
+        Store: 'East',
+        BookGenre: 'Fiction',
+        Author: 'John Doe',
+        UnitsSold: 3,
+        Revenue: 45
+      },
+      {
+        Date: '2024-01-07',
+        Store: 'West',
+        BookGenre: 'Non-Fiction',
+        Author: 'Jane Doe',
+        UnitsSold: 5,
+        Revenue: 125
+      },
+      {
+        Date: '2024-01-08',
+        Store: 'West',
+        BookGenre: 'Fiction',
+        Author: 'Emily White',
+        UnitsSold: 2,
+        Revenue: 30
+      },
+      {
+        Date: '2024-01-09',
+        Store: 'North',
+        BookGenre: 'Non-Fiction',
+        Author: 'James Smith',
+        UnitsSold: 2,
+        Revenue: 50
+      },
+      {
+        Date: '2024-01-10',
+        Store: 'South',
+        BookGenre: 'Fiction',
+        Author: 'John Doe',
+        UnitsSold: 4,
+        Revenue: 60
+      },
+      {
+        Date: '2024-01-11',
+        Store: 'East',
+        BookGenre: 'Non-Fiction',
+        Author: 'Jane Doe',
+        UnitsSold: 1,
+        Revenue: 27.5
+      },
+      {
+        Date: '2024-01-12',
+        Store: 'West',
+        BookGenre: 'Fiction',
+        Author: 'Emily White',
+        UnitsSold: 5,
+        Revenue: 75
+      },
+      {
+        Date: '2024-01-13',
+        Store: 'North',
+        BookGenre: 'Non-Fiction',
+        Author: 'James Smith',
+        UnitsSold: 3,
+        Revenue: 75
+      },
+      {
+        Date: '2024-01-14',
+        Store: 'East',
+        BookGenre: 'Fiction',
+        Author: 'John Doe',
+        UnitsSold: 2,
+        Revenue: 30
+      },
+      {
+        Date: '2024-01-15',
+        Store: 'South',
+        BookGenre: 'Non-Fiction',
+        Author: 'Jane Doe',
+        UnitsSold: 4,
+        Revenue: 110
+      },
+      {
+        Date: '2024-01-16',
+        Store: 'West',
+        BookGenre: 'Fiction',
+        Author: 'Emily White',
+        UnitsSold: 1,
+        Revenue: 15
+      },
+      {
+        Date: '2024-01-17',
+        Store: 'North',
+        BookGenre: 'Non-Fiction',
+        Author: 'James Smith',
+        UnitsSold: 5,
+        Revenue: 125
+      },
+      {
+        Date: '2024-01-18',
+        Store: 'East',
+        BookGenre: 'Fiction',
+        Author: 'John Doe',
+        UnitsSold: 3,
+        Revenue: 45
+      },
+      {
+        Date: '2024-01-19',
+        Store: 'South',
+        BookGenre: 'Non-Fiction',
+        Author: 'Jane Doe',
+        UnitsSold: 2,
+        Revenue: 55
+      },
+      {
+        Date: '2024-01-20',
+        Store: 'West',
+        BookGenre: 'Fiction',
+        Author: 'Emily White',
+        UnitsSold: 4,
+        Revenue: 60
+      }
+    ]
+  })
+  const [state, setState] = React.useState({})
+
+  Retool.useComponentSettings({
+    defaultHeight: 60,
+    defaultWidth: 20
+  })
+
+  return (
+    <div>
+      <PivotTableUI
+        data={pivotData}
+        // Here onChange updates the state with the new state received from PivotTableUI component.
+        onChange={(newState) => setState(newState)}
+        // The spread syntax is used to pass down the state as props to the PivotTableUI component.
+        {...state}
+      />
+    </div>
+  )
+}
